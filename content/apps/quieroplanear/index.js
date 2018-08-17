0c7528720dc89b4b1fa584642851f68b2f0c46be
Javi Moreno <jmoreno.zn@gmail.com>
2018-08-17 02:57:37 +0200

    anda, que si despues de lo que nos ha costado no funciona el if...

--- content/apps/quieroplanear/index.js
+++ content/apps/quieroplanear/index.js
@@ -6,10 +6,14 @@ QuieroPlanear = App.extend({
 		activate: function () {
 		    console.log('QuieroPlanear: activate()');
 		    this.app.helpers.register('disqus_subdomain', this.handleDisqusSubdomain);
+		    this.app.helpers.register('is_there_any_disqus_subdomain', this.handleIsThereAnyDisqusSubdomain);
 		},
 		handleDisqusSubdomain: function() {
 				var disqus_subdomain = process.env.DISQUS_SUBDOMAIN
 		    return(disqus_subdomain);
+		},
+		handleIsThereAnyDisqusSubdomain: function() {
+				return (!handleDisqusSubdomain() || /^\s*$/.test(handleDisqusSubdomain()));
 		}
 		
 });
