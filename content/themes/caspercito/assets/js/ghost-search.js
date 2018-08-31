2b63013bd5139aa8e9cdbe8b2215f274093d84ab
Javi Moreno <jmoreno.zn@gmail.com>
2018-08-31 10:18:24 +0200

    venga

--- content/themes/caspercito/assets/js/ghost-search.js
+++ content/themes/caspercito/assets/js/ghost-search.js
@@ -2,5 +2,6 @@ $("#search-field").ghostHunter({
     results   : "#results",
     onKeyUp  : true,
     zeroResultsInfo : false,
+    displaySearchInfo : false,
     result_template : "<a id='gh-{{ref}}' class='gh-search-item' href='{{link}}'><p><h2>{{title}}</h2><h4>{{pubDate}}</h4></p></a>"
 });
